c28fbdfd9baef6e2ecea12232d627c904e640cf5
GBTC441002UR <GBTC441002UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-06-13 09:58:01 -0400

    first commit

---
+++ src/StringAssignment.java
@@ -0,0 +1,15 @@
+
+
+//Write a Java program that assigns your name to a variable
+//and prints the variable to the console.
+
+
+    public class StringAssignment {
+    public static void main(String [] args)
+
+    {
+        String myName = "Yun Chen";
+        System.out.println("My name is " + myName +".");
+
+    }
+}
